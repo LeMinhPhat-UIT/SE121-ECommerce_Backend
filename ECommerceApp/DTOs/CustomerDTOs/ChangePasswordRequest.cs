84d589e9fc5cffce83cbd2891c05101ed11b4bd6
Manh Tran Quang <manhtq0508@gmail.com>
2026-04-27 14:42:40 +0700

    Add DTO for customer

---
+++ ECommerceApp/DTOs/CustomerDTOs/ChangePasswordRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceApp.DTOs.CustomerDTOs
+{
+    public class ChangePasswordRequest
+    {
+        [Required(ErrorMessage = "CustomerId is required.")]
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Current Password is required.")]
+        public string CurrentPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "New Password is required.")]
+        [MinLength(8, ErrorMessage = "New Password must be at least 8 characters.")]
+        public string NewPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "Confirm New Password is required.")]
+        [Compare("NewPassword", ErrorMessage = "New Password and Confirm New Password do not match.")]
+        public string ConfirmNewPassword { get; set; } = null!;
+    }
+}
---
+++ ECommerceApp/DTOs/CustomerDTOs/LoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceApp.DTOs.CustomerDTOs
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
+        public string Password { get; set; } = null!;
+    }
+}
