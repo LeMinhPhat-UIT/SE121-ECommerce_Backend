fa218d9ed1382fdcd6b4a6c17a327fff3c83414f
Manh Tran Quang <manhtq0508@gmail.com>
2026-04-27 14:42:11 +0700

    Add DTO for order

---
+++ ECommerceApp/DTOs/OrderDTOs/OrderResponse.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceApp.Enums;
+
+namespace ECommerceApp.DTOs.OrderDTOs
+{
+    public class OrderResponse
+    {
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CustomerId { get; set; }
+        public int BillingAddressId { get; set; }
+        public int ShippingAddressId { get; set; }
+        public decimal TotalBaseAmount { get; set; }
+        public decimal TotalDiscountAmount { get; set; }
+        public decimal ShippingCost { get; set; }
+        public decimal TotalAmount { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public List<OrderItemResponse> OrderItems { get; set; }
+    }
+}
