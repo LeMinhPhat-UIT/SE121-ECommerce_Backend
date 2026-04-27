884bfdc12fc336bc4de48352586f459f8fed7c1c
Manh Tran Quang <manhtq0508@gmail.com>
2026-04-27 14:43:07 +0700

    Add DTO for category

---
+++ ECommerceApp/DTOs/CategoryDTOs/CategoryResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceApp.DTOs.CategoryDTOs
+{
+    public class CategoryResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
