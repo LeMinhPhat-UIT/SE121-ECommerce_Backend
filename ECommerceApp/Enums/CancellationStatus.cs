c7c0104ee73d46bb17bcc706d1c85a208752225b
Manh Tran Quang <manhtq0508@gmail.com>
2026-04-27 14:29:58 +0700

    Add enums

---
+++ ECommerceApp/Enums/CancellationStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ECommerceApp.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum CancellationStatus
+    {
+        Pending = 1,
+        Approved = 8,
+        Rejected = 9
+    }
+}
