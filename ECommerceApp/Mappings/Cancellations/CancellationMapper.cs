3cd60f4f1c64e03a39724cbfbae8a33d281b9faa
Manh Tran Quang <manhtq0508@gmail.com>
2026-04-30 21:23:56 +0700

    Add mapping for cancellation

---
+++ ECommerceApp/Mappings/Cancellations/CancellationMapper.cs
@@ -0,0 +1,17 @@
+using ECommerceApp.DTOs.CancellationDTOs;
+using ECommerceApp.Entities;
+using Riok.Mapperly.Abstractions;
+
+namespace ECommerceApp.Mappings.Cancellations
+{
+    public interface ICancellationMapper
+    {
+        CancellationResponseDTO Map(Cancellation source);
+    }
+
+    [Mapper]
+    public partial class CancellationMapper : ICancellationMapper
+    {
+        public partial CancellationResponseDTO Map(Cancellation source);
+    }
+}
